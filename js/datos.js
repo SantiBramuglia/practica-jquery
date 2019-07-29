da9a7f424f509c1bf17485c1fc481fb276e9f466
SantiBramuglia <42210398+SantiBramuglia@users.noreply.github.com>
2019-07-29 15:41:42 -0300

    Add files via upload

--- js/datos.js
+++ js/datos.js
@@ -1,23 +1,9 @@
 // Sacado de https://api.github.com/users/joacomf
 // Sacado de https://api.github.com/users/joacomf/repos
 
-var usuario = {
-    "login": "joacomf",
-    "id": 4270154,
-    "avatar_url": "https://avatars2.githubusercontent.com/u/4270154?v=4",
-    "html_url": "https://github.com/joacomf",
-    "name": "Joaco Moreno",
-    "company": "Grupo Esfera",
-    "blog": "morenofernandez.com.ar",
-    "public_repos": 11,
-    "public_gists": 1,
-    "followers": 1,
-    "following": 1,
-    "created_at": "2013-04-27T02:48:23Z",
-    "updated_at": "2019-07-13T00:29:48Z"
-};
 
-var repositorios = [
+
+var repositorios2 = [
     {
     "id": 195335552,
     "name": "ci-cerradura",
--- js/script.js
+++ js/script.js
@@ -1,3 +1,91 @@
+var usuario;
+var repositorios;
+var contador = 0; 
 $(document).ready(function(){
-    alert(usuario.login);
+	
+	$("#boton_cargar").click(function(){
+		
+		
+		$.ajax({
+		method: "GET",
+		url: "https://api.github.com/users/" + $("#inputusser").val()
+		})
+		.done(function( respuestGIT) {
+		usuario = respuestGIT;
+		
+		
+		
+		
+		$("#nombre_usuario").text(usuario.name);
+		$("#id").text(usuario.id);
+		$("#nombre_empresa").text(usuario.company);
+		$("#numero_repositorios").text(usuario.public_repos);
+		$("#numero_gits").text(usuario.public_gists);
+		$("#numero_seguidores").text(usuario.following);
+		$("#numero_seguidos").text(usuario.followers);
+		$("#avatar").attr("src", "https://avatars2.githubusercontent.com/u/4270154?v=4");
+	});
+		$.ajax({
+		method: "GET",
+		url: 'https://api.github.com/users/'+ $("#inputusser").val()+ '/repos'
+		})
+		.done(function( respuestGITRepo) {
+		repositorios = respuestGITRepo;
+		
+		
+		var contenido ="";
+		for (var i=0; i<repositorios.length;i++){
+			contenido += '<tr><td class="dato" id="ID_repositorio">'+ repositorios[i].id +'</td><td class="dato" id="repositorio">'+ repositorios[i].name +'</td><td class="dato" id="descripcion">'+ repositorios[i].description +'</td></tr>';
+			}
+			$("#cuerpo_table").html(contenido);
+	
+
+		});
+			
+	
+})
+
+	
+	$("#numero_seguidos").click(function(){
+		$("#numero_seguidos").css("color", "red");
+	
+	})
+	$("#numero_seguidores").click(function(){
+		$("#numero_seguidores").css("color", "red");
+	
+	})
+	$(".dato").click(function(){
+		contador++;
+		$("#clicks_en_dato").text(contador);
+	
+	})
+	$("#avatar").hover(function(){
+			$("#avatar").animate({opacity:1},100);
+			}).mouseout(function(){
+			$("#avatar").animate({opacity:0.4},100);
+	});
+	$("#boton_reiniciar").click(function(){
+		
+		$("#nombre_usuario").text("-");
+		$("#id").text("-");
+		$("#nombre_empresa").text("-");
+		$("#numero_repositorios").text("-");
+		$("#numero_gits").text("-");
+		$("#numero_seguidores").text("-");
+		$("#numero_seguidos").text("-");
+		$("#avatar").attr("src", "https://cdn.iconscout.com/icon/free/png-256/avatar-372-456324.png");
+		var contenido ="";
+		for (var i=0; i<2;i++){
+			contenido += '<tr><td class="dato" id="ID_repositorio">'+ "-" +'</td><td class="dato" id="repositorio">'+ "-" +'</td><td class="dato" id="descripcion">'+ "-" +'</td></tr>';
+			}
+			$("#cuerpo_table").html(contenido);
+	})
+	$("#cuerpo_table").click(function(){
+		alert("EROR ERROR ERROR ERROR");
+		$("#cuerpo_table").css("color", "red");
+	
+	})
+	
+
+	
 })
